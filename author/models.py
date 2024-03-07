810428a899d9cbc64571a0ce9d8f07512bf8ac0a
sam <aquaticcalf@proton.me>
2024-03-07 21:15:06 +0530

    meow

--- author/models.py
+++ author/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.CharField(max_length=500)
+    photo = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
