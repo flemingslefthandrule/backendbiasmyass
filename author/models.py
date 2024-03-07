6316b84b40f5a6d33942a5fa6b2ea34df7b9607d
sam <aquaticcalf@proton.me>
2024-03-07 21:48:42 +0530

    meow

--- author/models.py
+++ author/models.py
@@ -1,9 +1,21 @@
 from django.db import models
+from django.utils.text import slugify
+from django.urls import reverse
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
     bio = models.CharField(max_length=500)
     photo = models.CharField(max_length=100)
+    url = models.SlugField(unique=True,max_length=100)
+
+    def url_make(self)->self:
+        return reverse(
+            "what_author",
+            kwargs={
+                "author_id" = self.id,
+                "url" = self.url
+            }
+        )
 
     def __str__(self):
         return self.name
