242d0a7890d329a5a089a326d360fc31b596f7b4
sam <aquaticcalf@proton.me>
2024-03-07 21:24:13 +0530

    meow

--- review/models.py
+++ review/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.dbapi import settings # todo : add user
+from author.models import Author
 
-# Create your models here.
+class review(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    # user = ????
+    text = models.TextField()
