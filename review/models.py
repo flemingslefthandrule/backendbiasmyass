44ab6ad3a7347e45f262a67c1328c234fa8c890a
sam <aquaticcalf@proton.me>
2024-03-08 11:16:44 +0530

    meow

--- review/models.py
+++ review/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings # todo : add user
 from author.models import Author
 
-class review(models.Model):
+class Review(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     # user = ????
     text = models.TextField()
--- user/admin.py
+++ user/admin.py
@@ -1,11 +1,4 @@
-from django.apps import apps
+from .models import User
 from django.contrib import admin
 
-
-models = apps.get_models()
-
-for model in models:
-    if admin.sites.AlreadyRegistered:
-        pass
-    else:
-        admin.site.register(model)
+admin.site.register(User)
