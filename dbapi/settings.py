1bde11b6b0abf18a10ac332c279360e8b812cfa3
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-03-08 18:08:02 +0530

    okay

--- dbapi/settings.py
+++ dbapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ep%x0%o5v4dk!sww9uf!lv$v&skn06_uc294zi9%sh@obno3g^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", 'localhost']
 
 
 # Application definition
